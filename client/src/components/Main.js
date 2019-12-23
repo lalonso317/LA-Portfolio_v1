76578e1802aa2c8a63964823a40006ec256a3dc7
Luis Alonso <if.create702@gmail.com>
2019-12-22 17:05:41 -0800

    adjusting the styling

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,9 +3,10 @@ import ContactUs from "./Email"
 import Icon from '../lib/Icon'
 // import image from '../../public/blur.jpg'
 import NBA from "../Assets/nbamemory.png"
-import BAR from "../Assets/pubdog.png"
+// import BAR from "../Assets/pubdog.png"
 import ETSY from "../Assets/estyclone.png"
 import ORDERUP from "../Assets/OrderUp.png"
+import GREYSCALE from "../Assets/greyscaleComputer.jpg"
 export default function Main(props){
     return(
         <div className="app">
@@ -27,67 +28,72 @@ export default function Main(props){
             </header>
             <main>
                 <div id="home" className="firstslide">
-                    <div className="myname">
-                        <h1>Luis Alonso</h1>
+                    <div className="greyBack">
+                    <img className="greyscale" src={GREYSCALE}></img>
                     </div>
-                    <div>
-                        <p>Full Stack Engineer &  A Student of Code</p>
+                    <div className="nameandtag">
+                        <div className="myname">
+                            <h1 className="fullname">Luis Alonso</h1>
+                        </div>
+                        <div>
+                            <p className="tagline">Full Stack Engineer &  A Student of Code</p>
+                        </div>
                     </div>
                 </div>
                 <div id="projects" className="secondslide">
                     <div className="projects">
                         <p className="project">Projects</p>
                         <div className="languages">
-                            <img className="code" src={"/html5.png"} ></img>
+                            {/* <img className="code" src={"/html5.png"} ></img>
                             <img className="code" src={"/css.png"}></img>
                             <img className="code" src={"/javascript.png"} ></img>
                             <img className="code" src={"/jquery.png"}></img>
                             <img className="code" src={"/react.png"}></img>
                             <img className="code" src={"/nodejs.png"}></img>
                             <img className="codes" src={"/mongodb-logo.png"}></img>
-                            <img className="codes" src={"/mysql=logo.png"}></img>
+                            <img className="codes" src={"/mysql=logo.png"}></img> */}
                         </div>
                         <div className="screenshots">
                             <div className="nbadiv">
                                 <label>NBA Memory Game</label>
-                                <a href="https://nbamemorye.surge.sh/" target="_blank"><img className="shot" src={NBA}></img></a>
-                            </div>
-                            <div className="nbadiv" >
-                                <label>Pub Dog Bar</label>
-                            <a href="https://pubdogbar.surge.sh/" target="_blank"><img className="barshot" src={BAR}></img></a>
+                                <div className="singleProject">
+                                    <a className="projectA" href="https://nbamemorye.surge.sh/" target="_blank"><img className="shot" src={NBA}></img></a>
+                                    <a className="projectGit" href="https://github.com/lalonso317/MemoryGame" target="_blank"><Icon className="icon" icon="github"></Icon></a>
+                                </div>
                             </div>
                             <div className="nbadiv">
                                 <label>Etsy Clone</label>
-                                <a href="https://estyclones.surge.sh/" target="_blank"><img className="shot" src={ETSY}></img></a>
+                                <div className="singleProject">
+                                    <a className="projectA" href="https://estyclones.surge.sh/" target="_blank"><img className="shot" src={ETSY}></img></a>
+                                    <a className="projectGit" href="https://github.com/lalonso317/Create-Etsy-Search-Page" target="_blank"><Icon className="icon"  icon="github"></Icon></a>
+                                </div>
                             </div>
                             <div className="nbadiv">
                                 <label>OrderUp.</label>
-                                <a href="#"><img className="ordershot" src={ORDERUP}></img></a>
+                                <div className="singleProject">
+                                    <a className="projectA" href="#"><img className="shot" src={ORDERUP}></img></a>
+                                    <a className="projectGit" href="https://github.com/lalonso317/RecipeBookMaster" target="_blank"><Icon className="icon"  icon="github"></Icon></a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <span id="about" ></span>
-                <div className="thirdslide">
-                    <div className="about">
-                        <p className="aboutme">About Me</p>
-                        <div>
-                            <p className="aboutText">Hi, I am only beginning my career as a Full Stack Engineer. With only 3 months of experience, I believe that I am able to perform at a high level. If you want to learn more, feel free to <a href="#contact">contact me.</a></p>
-                            <p className="aboutText">If you perfer paper, here is my resume</p>
-                            <a href="/resume2.pdf" target="_blank">Click here</a>
-                        </div>
-                    </div>
-                
-                </div>
                 <div id="contact" className="fourthslide">
                     <div className="contact">
                         <p className="contactme">Contact</p>
+                        <div>
+                            <p className="aboutText">Hi, I am only beginning my career as a Full Stack Engineer. With only 3 months of experience, I believe that I am able do some solid work. If you want to learn more, feel free to contact me down below.</p>
+                            <p className="aboutText">If you perfer paper, here is my resume</p>
+                            <a className="resume" href="/resume2.pdf" target="_blank">Click here</a>
+                        </div>
                     </div>
                     <div>
                         <div>
+                            <br></br>
                            <a href="mailto:if.create702@gmail.com"><p className="email">if.create702@gmail.com</p></a> 
-                           <p className="formtext">Thank you for taking the time to view my resume. If you would like to get in contact with me. You can either send me an email or fill out the form below and I will get back to you as soon as possible!</p>
-                            <ContactUs />
+                           <p className="formtext">Thank you again for taking the time to view my portfilo. If you would like to get in contact with me. You can send me an email and I will get back to you as soon as possible!</p>
+                        <br></br>
                         </div>
                     </div>
                 </div>
