97e2f99cb7e8259df85ab3efd514a1dec825a72d
Luis Alonso <if.create702@gmail.com>
2019-12-27 23:01:07 -0800

    Updates

---
+++ precache-manifest.2050d3d12f141cb7c83d02acd9765e40.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28f821028020b9901bca9719843ba1e",
+    "url": "/LA-Portfolio_v1/index.html"
+  },
+  {
+    "revision": "78c9bbfc86d8e1781211",
+    "url": "/LA-Portfolio_v1/static/css/2.f42df4cd.chunk.css"
+  },
+  {
+    "revision": "53ad7498b3c145bcd2ed",
+    "url": "/LA-Portfolio_v1/static/css/main.3a41ac17.chunk.css"
+  },
+  {
+    "revision": "78c9bbfc86d8e1781211",
+    "url": "/LA-Portfolio_v1/static/js/2.19713fef.chunk.js"
+  },
+  {
+    "revision": "53ad7498b3c145bcd2ed",
+    "url": "/LA-Portfolio_v1/static/js/main.4a8de161.chunk.js"
+  },
+  {
+    "revision": "e29d1b8575fd2c78ef83",
+    "url": "/LA-Portfolio_v1/static/js/runtime-main.22833752.js"
+  },
+  {
+    "revision": "fe6f85d6a670eea39c93239fa2297176",
+    "url": "/LA-Portfolio_v1/static/media/OrderUp.fe6f85d6.png"
+  },
+  {
+    "revision": "7e2ed43bfc2b90d050c7337859599ae5",
+    "url": "/LA-Portfolio_v1/static/media/estyclone.7e2ed43b.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/LA-Portfolio_v1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/LA-Portfolio_v1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/LA-Portfolio_v1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/LA-Portfolio_v1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/LA-Portfolio_v1/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "620b2e917bb7019fb9cd327356824e29",
+    "url": "/LA-Portfolio_v1/static/media/greyscaleComputer.620b2e91.jpg"
+  },
+  {
+    "revision": "9f9b4f38e959de9e6f82a56aebd2c4fa",
+    "url": "/LA-Portfolio_v1/static/media/nbamemory.9f9b4f38.png"
+  },
+  {
+    "revision": "95d5d238bb10b84a12e197a257417d0b",
+    "url": "/LA-Portfolio_v1/static/media/resume2.95d5d238.pdf"
+  }
+]);
